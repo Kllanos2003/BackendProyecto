0120db09420efc4d692abba66a50f107bb6c2796
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2023-09-12 19:04:38 -0500

    Primer commit clase 12/09/2023

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'HOME PAGE';
+});
+
+Route::get('producto', function () {
+    return "LISTADO DE TODOS LOS PRODUCTOS";
+});
+
+Route::get('producto/create', function () {
+    return "AQUI VA EL FORMULARIO DE LA CREACION DE PRODUCTOS";
+});
+
+
+Route::get('producto/{id}', function ($id) {
+    return "el nombre del producto es $id";
+});
+
+Route::get('producto/{id}/{categoria}', function ($id, $categoria) {
+    return "el nombre del producto es $id con la categoria $categoria";
+});
+
